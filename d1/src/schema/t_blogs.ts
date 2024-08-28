56ba1ebcf2c2bdcc09196e4f921fcf2d651ba2d3
darkqiank <darkqiank@foxmail.com>
2024-08-28 09:03:51 +0800

    修复插入模式

--- d1/src/schema/t_blogs.ts
+++ d1/src/schema/t_blogs.ts
@@ -15,28 +15,13 @@ export const t_blogs = sqliteTable('t_blogs', {
 export const getLatestBlogs = async (env: Env) => {
     const db = createDbInstance(env);
     // return await db.select().from(t_blogs).all();
-	return await db.select().from(t_blogs).where(sql`${t_blogs.link} >= date('now', '-7 days')`).all();
+	return await db.select().from(t_blogs).where(sql`${t_blogs.link} >= date('now', '-30 days')`).all();
 };
 
-export const createOrUpdateBlog = async (env: Env, link: string, source: string) => {
-    const db = createDbInstance(env);
-	const existingBlog = await db.select().from(t_blogs).where(sql`${t_blogs.link} = ${link}`).get()
-	if (existingBlog) {
-        // 如果存在，则更新现有条目
-		// console.log("更新新条目")
-        await db.update(t_blogs).set({ source }).where(sql`${t_blogs.link} = ${link}`).run();
-    } else {
-        // 如果不存在，则插入新条目
-		// console.log("插入新条目")
-        await db.insert(t_blogs).values({ link, source }).run();
-    }
-};
 
 export const batchUpdateBlog = async (env: Env, blogs: { link: string, source: string }[]) => {
-	for (const blog of blogs) {
-		// console.log(blog)
-		await createOrUpdateBlog(env, blog.link, blog.source)
-	}
+	const db = createDbInstance(env);
+	return await db.insert(t_blogs).values(blogs).onConflictDoNothing()
 }
 
 // Read blog entry by Date
