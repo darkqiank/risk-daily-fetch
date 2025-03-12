8261298618b6fdf64867c82939702f9aad612952
darkqiank <darkqiank@foxmail.com>
2025-03-12 15:50:29 +0800

    修复tailwind

--- next-fronted/tailwind.config.js
+++ next-fronted/tailwind.config.js
@@ -1,12 +1,9 @@
-import {nextui} from '@nextui-org/theme'
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './layouts/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -17,5 +14,5 @@ module.exports = {
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [],
 }
