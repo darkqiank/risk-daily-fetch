025177c1ec8e6e183daf67916ad54ed21023ca29
darkqiank <darkqiank@foxmail.com>
2025-04-15 17:46:58 +0800

    新增post接口

--- next-fronted/pages/api/detail/index.ts
+++ next-fronted/pages/api/detail/index.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-import { DetailFilters, getPaginatedData } from "@/db/schema/content_detail";
+import { batchInsertContentDetail, DetailFilters, getPaginatedData } from "@/db/schema/content_detail";
 import searchClient, { extractHighlightSnippets } from "@/db/search";
+import { authenticate } from "@/components/auth";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -27,30 +27,34 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
       let searchHits: Array<{ id: string; snippet: string }> = [];
 
+      // if (typeof query === "string" && query.trim()) {
+      //   const searchResult = await searchClient
+      //     .index("meilisearch_index_content_detail")
+      //     .search(query, {
+      //       attributesToHighlight: ["*"],
+      //       highlightPreTag: "__hl__",
+      //       highlightPostTag: "__/hl__",
+      //       limit: 1000,
+      //     });
+
+      //   searchHits = searchResult.hits.map((item: any) => ({
+      //     id: item.id,
+      //     snippet: extractHighlightSnippets(
+      //       [
+      //         item._formatted?.url ?? "",
+      //         item._formatted?.source ?? "",
+      //         item._formatted?.content ?? "",
+      //       ]
+      //         .filter(Boolean) // 自动过滤空字符串
+      //         .join(" ... "), // 用空格连接非空内容
+      //     ),
+      //   }));
+
+      //   filters.ids = searchHits.map((item) => item.id);
+      // }
+
       if (typeof query === "string" && query.trim()) {
-        const searchResult = await searchClient
-          .index("meilisearch_index_content_detail")
-          .search(query, {
-            attributesToHighlight: ["*"],
-            highlightPreTag: "__hl__",
-            highlightPostTag: "__/hl__",
-            limit: 1000,
-          });
-
-        searchHits = searchResult.hits.map((item: any) => ({
-          id: item.id,
-          snippet: extractHighlightSnippets(
-            [
-              item._formatted?.url ?? "",
-              item._formatted?.source ?? "",
-              item._formatted?.content ?? "",
-            ]
-              .filter(Boolean) // 自动过滤空字符串
-              .join(" ... "), // 用空格连接非空内容
-          ),
-        }));
-
-        filters.ids = searchHits.map((item) => item.id);
+        filters.search = query;
       }
 
       if (typeof date === "string") {
@@ -86,19 +90,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       res_data = await getPaginatedData(filters, pn, ps);
 
       // 合并高亮信息
-      if (searchHits.length > 0) {
-        const hitMap = new Map(searchHits.map((hit) => [hit.id, hit]));
+      // if (searchHits.length > 0) {
+      //   const hitMap = new Map(searchHits.map((hit) => [hit.id, hit]));
 
-        res_data = {
-          ...res_data,
-          data: res_data.data.map((item: any) => ({
-            ...item,
-            snippet: hitMap.get(item.id)?.snippet || null, // 改为数组
-          })),
-        };
-      }
+      //   res_data = {
+      //     ...res_data,
+      //     data: res_data.data.map((item: any) => ({
+      //       ...item,
+      //       snippet: hitMap.get(item.id)?.snippet || null, // 改为数组
+      //     })),
+      //   };
+      // }
 
       res.status(200).json(res_data);
+    } else if (req.method === "POST") {
+      authenticate(req);
+      const data  = req.body;
+      console.log(data);
+      if (Array.isArray(data)) {
+        await batchInsertContentDetail(data);
+      } else {
+        res.status(400).json({ message: "Invalid data format" });
+      }
+      res.status(200).json({ message: "Data inserted successfully" });
     } else {
       res.status(405).json({ message: "Method not allowed" });
     }
