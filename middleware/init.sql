932954376f32867e6d0236011bf939cca47eeffa
darkqiank <darkqiank@foxmail.com>
2025-07-03 15:17:17 +0800

    新增大模型网关

---
+++ middleware/init.sql
@@ -0,0 +1,11 @@
+-- init.sql
+
+CREATE DATABASE litellm;
+CREATE DATABASE prefect;
+
+-- 可选：创建专用用户并授权
+-- CREATE USER litellm_user WITH PASSWORD 'your_secure_password';
+-- GRANT ALL PRIVILEGES ON DATABASE litellm TO litellm_user;
+
+-- CREATE USER prefect_user WITH PASSWORD 'your_secure_password';
+-- GRANT ALL PRIVILEGES ON DATABASE prefect TO prefect_user;
